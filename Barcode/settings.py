6d88b8fe17118ade868fa97cf1eff91e7b69a59f
nihattadilov <adilov.nihatt@gmail.com>
2025-05-14 15:41:44 +0400

    commentss

--- Barcode/settings.py
+++ Barcode/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -52,7 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -87,45 +88,50 @@ WSGI_APPLICATION = "Barcode.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+
 
 
 from decouple import config
 import dj_database_url
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
-
+DB_LIVE=os.getenv("DB_LIVE")
 
 # DATABASES = {
-
-#     'default': {
-
-#         'ENGINE': 'django.db.backends.postgresql',
-
-#         'NAME': os.getenv('POSTGRES_DB'),
-
-#         'USER': os.getenv('POSTGRES_USER'),
-
-#         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-
-#         'HOST': os.getenv('POSTGRES_HOST'),
-
-#         'PORT': os.getenv('POSTGRES_PORT'),
-
-#     }
-
+#     'default': dj_database_url.config(
+#         default=config('DATABASE_URL'),
+#         conn_max_age=600,
+#         ssl_require=True
+#     )
 # }
+if DB_LIVE in ["False",False]:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+
+else:
+    DATABASES = {
+
+        'default': {
+
+            'ENGINE': 'django.db.backends.postgresql',
+
+            'NAME': os.getenv("DB_NAME"),
+
+            'USER': os.getenv("DB_USER"),
+
+            'PASSWORD': os.getenv("DB_PASSWORD"),
+
+            'HOST': os.getenv("DB_HOST"),
+
+            'PORT': os.getenv("DB_PORT"),
+
+        }
+
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
