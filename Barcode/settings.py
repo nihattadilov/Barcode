57d40eeae3f78c8fe8ba30576f1a3b6078f31a5a
nihattadilov <adilov.nihatt@gmail.com>
2025-05-15 10:29:11 +0400

    Update Procfile and add entrypoint.sh for automated depploy

--- Barcode/settings.py
+++ Barcode/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = os.getenv("DEBUG")
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 INSTALLED_APPS = [
