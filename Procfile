698b2fe9a79ebe2c063bb45d570d6ee2511f0252
nihattadilov <adilov.nihatt@gmail.com>
2025-05-15 10:14:57 +0400

    Update Procfile and add entrypoint.sh for automated deploy

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn Barcode.wsgi:application --bind 0.0.0.0:$PORT
+web: bash entrypoint.sh
---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "🔄 Running migrate..."
+python manage.py migrate
+
+echo "🧹 Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "🚀 Starting Gunicorn server..."
+gunicorn Barcode.wsgi:application --bind 0.0.0.0:$PORT
